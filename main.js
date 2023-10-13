e463c283616e07ba36f6404db97420c9d83a6861
Apillicus <106998574+Apillicus@users.noreply.github.com>
2023-10-13 11:56:33 -0500

    combined template.js and main.js

--- main.js
+++ main.js
@@ -1,4 +1,127 @@
 const Template = require('./template');
+const fs = require('fs');
+const PDFDocument = require('pdfkit');
+
+class Template {
+  constructor(dimensions, units) {
+    this.dimensions = dimensions;
+    this.units = units;
+    this.shapes = [];
+    this.patterns = [];
+    this.gridSize = 0;
+    this.tools = [];
+    this.settings = {};
+  }
+
+  save(name, details) {
+    fs.writeFileSync(`./templates/${name}.json`, JSON.stringify({ ...details, shapes: this.shapes, patterns: this.patterns, gridSize: this.gridSize, tools: this.tools, settings: this.settings }));
+  }
+
+  static load(id) {
+    const data = fs.readFileSync(`./templates/${id}.json`);
+    return new Template(JSON.parse(data));
+  }
+
+  exportToPDF(includeInstructions, includeNotes) {
+    const doc = new PDFDocument();
+    doc.pipe(fs.createWriteStream(`./exports/${this.name}.pdf`));
+    this.shapes.forEach(shape => shape.draw(doc));
+    if (includeInstructions) doc.text(this.instructions);
+    if (includeNotes) doc.text(this.notes);
+    doc.end();
+  }
+
+  drawShape(shape, dimensions) {
+    this.shapes.push({ shape, dimensions });
+  }
+
+  applyPattern(pattern) {
+    this.patterns.push(pattern);
+  }
+
+  enableSnapToGrid(gridSize) {
+    this.gridSize = gridSize;
+  }
+
+  exportDesign(includeMaterials, includeTools, includeCutList) {
+    const design = { shapes: this.shapes, patterns: this.patterns };
+    if (includeMaterials) design.materials = this.materials;
+    if (includeTools) design.tools = this.tools;
+    if (includeCutList) design.cutList = this.cutList;
+    fs.writeFileSync(`./exports/${this.name}.json`, JSON.stringify(design));
+  }
+
+  addStitchPunchPatterns(pattern) {
+    this.patterns.push(pattern);
+  }
+
+  selectPointCutPath(points) {
+    this.cutList = points;
+  }
+
+  addJoiningMarks(marks) {
+    this.joiningMarks = marks;
+  }
+
+  estimateMaterials() {
+    // Simple example: summing the areas of the shapes
+    const totalArea = this.shapes.reduce((sum, { shape, dimensions }) => {
+      // Example: Assuming the shape is a rectangle
+      if (shape === 'rectangle') {
+        return sum + dimensions.width * dimensions.height;
+      }
+      // Other shape calculations...
+      return sum;
+    }, 0);
+  
+    // Assuming a simple material estimation based on total area
+    this.materials = { 'totalArea': totalArea };
+  }
+
+  selectTools(tools) {
+    this.tools = tools;
+  }
+
+  preview() {
+    // This method would typically be implemented on the client-side
+    // Here's a pseudo-implementation
+    console.log("Rendering a preview of the shapes and patterns...");
+  }
+  
+
+  share(users) {
+    // Pseudo-code, this heavily depends on your application's infrastructure
+    users.forEach(user => {
+      console.log(`Sharing design with ${user}...`);
+      // Add database entry, send emails, or other sharing logic...
+    });
+  }
+  
+
+  customizeWorkspace(settings) {
+    this.settings = settings;
+  }
+
+  provideFeedback(feedback) {
+    fs.writeFileSync(`./feedback/${Date.now()}.txt`, feedback);
+  }
+
+}
+
+module.exports = Template;
+
+// This part of the code deals with UI interactions and uses the Template class
+document.addEventListener('DOMContentLoaded', (event) => {
+  const Template = require('./template'); 
+ 
+  let template; 
+
+  document.getElementById('newTemplate').addEventListener('click', function() {
+    const dimensions = {width: document.getElementById('width').value, height: document.getElementById('height').value};
+    const units = document.getElementById('units').value;
+    template = newTemplate(dimensions, units); // create a new template when the button is clicked
+  });
+
 
 // Function to create a new template
 function newTemplate(dimensions, units) {
@@ -151,7 +274,7 @@ function selectPointCutPath(template, points) {
     const points = prompt("Please enter the points for the cut path (e.g., 'x1,y1;x2,y2'):");
     selectPointCutPath(template, points.split(';').map(point => point.split(',').map(Number))); // Convert the input string to an array of points
   });
-  
+
 // Function to add joining marks
 function addJoiningMarks(template, marks) {
   template.addJoiningMarks(marks);
